90cb63d4d343fbaa879ed15fedc1ef34b376ac26
Jane Philipps <jane.philipps@gmail.com>
2017-10-16 21:43:15 -0700

    get static content to render on board

--- src/components/Board.js
+++ src/components/Board.js
@@ -4,8 +4,18 @@ import Square from './Square';
 
 export default class Board extends React.PureComponent {
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      board: [['X', 'O', ''],
+              ['', '', ''],
+              ['', '', '']]
+    }
+  }
+
   static propTypes = {
-    onClick: PropTypes.func
+    onClick: PropTypes.func,
+    content: PropTypes.string
   }
 
   _handleClick(e) {
@@ -17,19 +27,46 @@ export default class Board extends React.PureComponent {
     return (
       <div className="board">
         <div className="row">
-          <Square onClick={this._handleClick} />
-          <Square onClick={this._handleClick} />
-          <Square onClick={this._handleClick} />
+          <Square
+            onClick={this._handleClick}
+            content={this.state.board[0][0]}
+          />
+          <Square
+            onClick={this._handleClick}
+            content={this.state.board[0][1]}
+          />
+          <Square
+            onClick={this._handleClick}
+            content={this.state.board[0][2]}
+          />
         </div>
         <div className="row">
-          <Square onClick={this._handleClick} />
-          <Square onClick={this._handleClick} />
-          <Square onClick={this._handleClick} />
+          <Square
+            onClick={this._handleClick}
+            content={this.state.board[1][0]}
+          />
+          <Square
+            onClick={this._handleClick}
+            content={this.state.board[1][1]}
+          />
+          <Square
+            onClick={this._handleClick}
+            content={this.state.board[1][2]}
+          />
         </div>
         <div className="row">
-          <Square onClick={this._handleClick} />
-          <Square onClick={this._handleClick} />
-          <Square onClick={this._handleClick} />
+          <Square
+            onClick={this._handleClick}
+            content={this.state.board[2][0]}
+          />
+          <Square
+            onClick={this._handleClick}
+            content={this.state.board[2][1]}
+          />
+          <Square
+            onClick={this._handleClick}
+            content={this.state.board[2][2]}
+          />
         </div>
       </div>
     );
--- src/components/Square.js
+++ src/components/Square.js
@@ -4,7 +4,8 @@ import PropTypes from 'prop-types';
 export default class Square extends React.PureComponent {
 
   static propTypes = {
-    onClick: PropTypes.func
+    onClick: PropTypes.func,
+    content: PropTypes.string
   }
 
   _handleClick(e) {
@@ -17,8 +18,13 @@ export default class Square extends React.PureComponent {
 
   render() {
 
+    let {
+      content
+    } = this.props;
+
     return (
       <div className="square" onClick={this._handleClick.bind(this)}>
+        {content}
       </div>
     );
   }
