84d41bd2a994468ac4bc20599d9d68af6c62aa3d
Jane Philipps <jane.philipps@gmail.com>
2017-12-04 20:19:00 -0800

    add ability to add multiple boards

--- src/App.js
+++ src/App.js
@@ -6,11 +6,27 @@ import './App.css';
 
 export default class App extends React.PureComponent {
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      boards: [this._createBoard()]
+    };
+  };
+
   _addBoard() {
     console.log("adding board")
+    let boards = this.state.boards;
+    let newBoards = boards.concat([this._createBoard()]);
+    this.setState({boards: newBoards});
+  }
+
+  _createBoard() {
+    return <Board />
   }
 
   render() {
+
+    let { boards } = this.state;
 
     return (
       <div className="App">
@@ -20,7 +36,7 @@ export default class App extends React.PureComponent {
         <button onClick={this._addBoard.bind(this)}>
           NEW BOARD
         </button>
-        <Board />
+        {boards}
       </div>
     );
   }
